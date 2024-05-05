5912d5aad36ad5339a459b6c7c585ec7e64ed067
whakley <wesleyc1223@gmail.com>
2024-05-05 15:50:57 -0700

    added terrain

--- scripts/entities/player.cs
+++ scripts/entities/player.cs
@@ -5,7 +5,7 @@ public partial class player : EntityBase
 {
 
 	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float JumpVelocity = -800.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
