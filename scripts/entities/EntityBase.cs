f322914b3c6989e508e50ba91edf06d41ca636f6
Jeffrey Yeung <jyeungprivate@gmail.com>
2024-05-15 16:46:18 -0700

    w

--- scripts/entities/EntityBase.cs
+++ scripts/entities/EntityBase.cs
@@ -3,15 +3,12 @@ using System;
 
 public abstract partial class EntityBase : CharacterBody2D
 {
-
-protected int leftRight;
-protected int upDown;
-
+	protected int leftRight;
+	protected int upDown;
 	protected bool isJumping;
 	protected int jumpsLeft;
 	protected	float JumpVelocity;
 	protected float Speed;
-
 
 // TODO: move fastvalue, jump height and num of jumps ot export. idk how to and its late, and i want to go to bed, and i feel like playing games but its late and i shid.
 	protected float fastFallValue = 0.2f;
@@ -29,9 +26,7 @@ protected int upDown;
 	[Export] protected int Size;
 	[Export] protected int MoveSpeed;
 	[Export] protected int Resistance;
-
 	[Export] protected int jumpHeight;
-
 	[Export] protected int numOfJumps;
 	
 		public void die() {
@@ -54,8 +49,8 @@ public override void _Ready()
 		hpBar = GetNode<TextureProgressBar>("EntityHealthBar");
 		hpBar.Value = baseStats.getMaxHp();
 		hpBar.MaxValue = baseStats.getMaxHp();
-
 		baseStats.setHp(Health);
+		baseStats.setMaxHp(Health);
 		baseStats.setRegenPercent(RegenPercent);
 		baseStats.setAr(Armour);
 		baseStats.setMr(MagicResist);
@@ -68,22 +63,19 @@ public override void _Ready()
 		baseStats.setRes(Resistance);
 		baseStats.SetJumpHeight(jumpHeight);
 		baseStats.SetNumOfJumps(numOfJumps);
-		baseStats.setMaxHp(Health);
 		hpBar = GetNode<TextureProgressBar>("EntityHealthBar");
 		hpBar.MaxValue = baseStats.getMaxHp();
 		hpBar.Value = baseStats.getMaxHp();
-		GD.Print(hpBar.Value);
+		this.JumpVelocity = -1000;
+		this.numOfJumps = 2;
+		this.Speed = baseStats.getMoveSpeedScaler();
+
 	}
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
 	public override void _PhysicsProcess(double delta)
 	{
-
-
 		Vector2 velocity = Velocity;
-
-
 
 		// Add the gravity.
 		if (!IsOnFloor()){
@@ -96,13 +88,10 @@ public override void _Ready()
 		if (isJumping && jumpsLeft > 0){
 
 			velocity.Y = JumpVelocity;
-		}
-
-		
+		}	
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = new Vector2(leftRight, upDown);
-		
 
 		if (direction != Vector2.Zero)
 		{
@@ -125,16 +114,4 @@ public override void _Ready()
 /// <summary>
 /// edit the entities "lr" variable with this function to define how it moves left to right
 /// </summary>
-
-
-
-public override void _Process(double delta)
-	{
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
-		Console.WriteLine("process");
-
-	}
-
-	
-
 }
