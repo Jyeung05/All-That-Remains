a3404237cc67924998d3d6b23c53cb49688c9e02
whakley <wesleyc1223@gmail.com>
2024-06-01 17:13:43 -0700

    enemy updated

--- scripts/entities/trial_enemy.cs
+++ scripts/entities/trial_enemy.cs
@@ -1,6 +1,37 @@
 using Godot;
 using System;
 
+
 public partial class trial_enemy : enemy_base
-{
+{   
+	private const float SPEED = 100f;
+	/*private void moveToTarget(double delta)
+	{
+		if(this.isLeft){
+			this.leftRight = -1;
+		}
+		if(this.isRight){
+			this.leftRight = 1;
+		}
+		base._PhysicsProcess(delta);
+	}*/
+	
+	Area2D detectionArea;
+	public override void _Ready()
+	{
+		
+		target = GetNode<player>("../Player");
+		detectionArea = GetNode<Area2D>("./Detection Area");
+		base._Ready();
+	}
+	public override void _PhysicsProcess(double delta)
+	{   
+		this.Speed = SPEED;
+		if(inArea){
+				moveToTarget(target);
+		}
+		base._PhysicsProcess(delta);
+	}
+
 }
+
